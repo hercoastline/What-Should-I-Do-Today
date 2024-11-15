abddcacec578d70cc815ca4a8f9e4cff259daabe
hercoastline <lotta.tamminen@gmail.com>
2024-11-15 09:01:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ function switchEdition(edition) {
 function displayNewIdea() {
     const randomIdea = activities[Math.floor(Math.random() * activities.length)];
     document.getElementById("activityIdea").textContent = randomIdea;
-    document.getElementById("activityButton").textContent = "Not today, give me another idea";
+    document.getElementById("activityButton").textContent = "Give me another idea :)";
 }
 
 // Add event listeners for edition switch
